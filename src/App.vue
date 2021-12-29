b2c48d4003341bbe93b0ec1c7fff7217101c43ed
DESKTOP-5BSR2KC\YM <ymiru0324@naver.com>
2021-12-29 21:59:06 +0900

    code: update # 07

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="name">
+    {{ name }}
+  </div>
+  <button 
+    class="btn btn-primary"
+    v-on:click="updateName"
+  >
+    Click
+  </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const name = ref({
+      id: 1
+    });
+
+    // const greeting = (name) => {
+    //   return 'Hello, ' + name;
+    // };
+
+    // const greet = greeting(name);
+    const updateName =() => {
+      name.value.id = 2;
+      console.log(name);
+    };
+
+    return {
+      name,
+      updateName
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .name {
+    color:red;
+  }
 </style>
