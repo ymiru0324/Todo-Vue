6a3f7d83c1584956e9604758dd943354d6b5f31b
DESKTOP-5BSR2KC\YM <ymiru0324@naver.com>
2021-12-29 23:43:34 +0900

    code: update # 12

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="name">
-    {{ name }}
+  <div class="container">
+      <h2>To-Do List</h2>
+    <form 
+      @submit.prevent="onSubmit"
+      class="d-flex"
+    >
+      <div class="flex-grow-1 mr-2">
+        <input 
+          class="form-control"
+          type="text"
+          v-model="todo"
+          placeholder="Type new to-do"
+        >
+      </div>
+      <div>
+        <button 
+          class="btn btn-primary"
+          type="submit"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+    <div 
+      v-for="todo in todos"
+      :key="todo.id"
+      class="card mt-2"
+    >
+      <div class="card-body p-2">
+        {{ todo.subject }}
+      </div>
+    </div>
   </div>
-  <button 
-    class="btn btn-primary"
-    v-on:click="updateName"
-  >
-    Click
-  </button>
 </template>
 
 <script>
@@ -15,30 +39,29 @@ import { ref } from 'vue';
 
 export default {
   setup() {
-    const name = ref({
-      id: 1
-    });
+    const todo = ref('');
+    const todos = ref([
+      { id: 1, subject: '휴대폰 사기'},
+      { id: 2, subject: '장보기'},
+    ]);
 
-    // const greeting = (name) => {
-    //   return 'Hello, ' + name;
-    // };
-
-    // const greet = greeting(name);
-    const updateName =() => {
-      name.value.id = 2;
-      console.log(name);
+    const onSubmit = () => {
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value
+      });
     };
 
+
     return {
-      name,
-      updateName
+      todo,
+      todos,
+      onSubmit,
     };
   }
 }
 </script>
 
 <style>
-  .name {
-    color:red;
-  }
+
 </style>
