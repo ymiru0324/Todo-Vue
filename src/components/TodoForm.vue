fa94ad19a62bb9a89c6bc3ac1c185dae9fee9ce3
DESKTOP-5BSR2KC\YM <ymiru0324@naver.com>
2022-01-05 01:31:35 +0900

    code: update # 60

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,199 @@
+<template>
+  <div v-if="loading">
+    Loading..
+  </div>
+  <form 
+    v-else
+    @submit.prevent="onSave"
+  >
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>Subject</label>
+          <input 
+            v-model="todo.subject" 
+            type="text" 
+            class="form-control"
+          >
+          <div 
+            v-if="subjectError"
+            style="color: red"
+        >
+            {{ subjectError }}
+          </div>
+        </div>
+      </div>
+      <div v-if="editing" class="col-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <button 
+              class="btn"
+              type="button"
+              :class="todo.completed ? 'btn-success' : 'btn-danger'"
+              @click="toggleTodoStatus"
+            >
+              {{ todo.completed ? 'Completed' : 'Incomplete' }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-group">
+          <label>Body</label>
+            <textarea 
+                v-model="todo.body"
+                cols="30"
+                rows="10"
+                class="form-control"
+            >
+            </textarea>
+        </div>
+      </div>
+    </div>
+    <button 
+      type="submit" 
+      class="btn btn-primary"
+      :disabled="!todoUpdated"
+    >
+      {{ editing ? 'Update' : 'Create' }}
+    </button>
+    <button 
+      class="btn btn-outline-dark ml-2"
+      @click="moveToTodoListPage"
+    >
+      Cancel
+    </button>
+  </form>
+  <Toast 
+    v-if="showToast" 
+    :message="toastMessage"
+    :type="toastAlertType"
+  />
+</template>
+
+<script>
+import { useRoute, useRouter } from 'vue-router';
+import axios from 'axios';
+import { ref, computed } from 'vue';
+import _ from 'lodash';
+import Toast from '@/components/Toast.vue';
+import { useToast } from '@/composables/toast';
+
+export default {
+  components: {
+    Toast
+  },
+  props: {
+      editing: {
+          type: Boolean,
+          default: false
+      }
+  },
+    setup(props) {     
+        const route = useRoute();
+        const router = useRouter();
+        const todo = ref({
+            subject: '',
+            completed: false,
+            bodt: ''
+        });
+        const subjectError = ref('');
+        const originalTodo = ref(null);
+        const loading = ref(false);
+        const {
+          showToast,
+          toastMessage,
+          toastAlertType,
+          triggetToast
+        } = useToast();
+        
+        const todoId = route.params.id
+
+        const getTodo = async () => {
+            loading.value = true;
+          try {
+            const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
+            
+            todo.value = { ...res.data };
+            originalTodo.value = { ...res.data };
+            loading.value = false;
+          } catch (error) {
+            loading.value = false;
+            console.log(error);
+            triggetToast('Something went wrong', 'danger');
+          }
+        };
+
+        const todoUpdated = computed(() => {
+          return !_.isEqual(todo.value, originalTodo.value)
+        });
+
+        const toggleTodoStatus = () => {
+          todo.value.completed = !todo.value.completed;
+        };
+
+        const moveToTodoListPage = () => {
+          router.push({
+            name: 'Todos'
+          })
+        };
+
+        if (props.editing) {
+            getTodo();
+        }
+
+        const onSave = async () => {
+            subjectError.value = '';
+            if (!todo.value.subject) {
+                subjectError.value = 'Subject is required';
+                return;
+            }
+
+          try {
+            let res;
+            const data = {
+                subject: todo.value.subject,
+                completed: todo.value.completed,
+                body: todo.value.body,
+            };
+            if (props.editing) {
+                res = await axios.put(`
+                http://localhost:3000/todos/${todoId}
+                `, data);
+                originalTodo.value = { ...res.data };
+            } else {
+                res = await axios.post(`
+                http://localhost:3000/todos
+                `, data);
+                todo.value.subject = '';
+                todo.value.body = '';
+            }
+
+            const message = 'Successfully ' + (props.editing ? 'Updated!' : 'Created!');
+            triggetToast(message);
+          } catch (error) {
+            console.log(error);
+            triggetToast('Something went wrong', 'danger');
+          }
+        };
+
+        return {
+          todo,
+          loading,
+          toggleTodoStatus,
+          moveToTodoListPage,
+          onSave,
+          todoUpdated,
+          showToast,
+          toastMessage,
+          toastAlertType,
+          subjectError
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/todos/index.vue
+++ src/pages/todos/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h2>To-Do List</h2>
+    <div class="d-flex justify-content-between mb-3">
+      <h2>To-Do List</h2>
+      <button 
+        class="btn btn-primary"
+        @click="moveToCreatePage"
+      >
+        Create Todo
+      </button>
+    </div>
     <input 
       class="form-control"
       type="text"
@@ -9,11 +17,6 @@
       @keyup.enter="searchTodo"
     >
     <hr />
-
-    <TodoSimpleForm @add-todo="addTodo" />
-    <div style="color: red">
-      {{ error }}
-    </div>
 
     <div v-if="!todos.length">
       There is nothing to display
@@ -59,20 +62,20 @@
 
 <script>
 import { ref, computed, watch } from 'vue';
-import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
 import TodoList from '@/components/TodoList.vue';
 import axios from 'axios';
 import Toast from '@/components/Toast.vue';
 import { useToast } from '@/composables/toast';
+import { useRouter } from 'vue-router';
 
 export default {
   components: {
-    TodoSimpleForm,
     TodoList,
     Toast,
   },
 
   setup() {
+    const router = useRouter();
     const todos = ref([]);
     const error = ref('');
     const numberOfTodos = ref(0);
@@ -150,6 +153,12 @@ export default {
       }
     };
 
+    const moveToCreatePage = () => {
+      router.push({
+        name: 'TodoCreate',
+      })
+    };
+
     let timeout = null;
 
     const searchTodo = () => {
@@ -178,6 +187,7 @@ export default {
       toastMessage,
       toastAlertType,
       showToast,
+      moveToCreatePage,
     };
   }
 }
