34c17da52ad3d6c1243522cd9f976966dcdf9d46
DESKTOP-5BSR2KC\YM <ymiru0324@naver.com>
2022-01-05 22:35:04 +0900

    code: refactoring

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,55 +1,64 @@
 <template>
-  <div 
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      class="card mt-2"
+  <!-- <div 
+    v-for="(todo, index) in todos"
+    :key="todo.id"
+    class="card mt-2"
+  > -->
+    <List
+      :items="todos"
     >
-      <div 
-        class="card-body p-2 d-flex align-items-center"
-        style="cursor: pointer"
-        @click="moveToPage(todo.id)"
-      >
-        <div class="flex-grow-1">
-          <input
-            class="ml-2 mr-2"
-            type="checkbox"
-            :checked="todo.completed"
-            @change="toggleTodo(index, $event)"
-            @click.stop
-          >
-          <span 
-            :class="{ todo: todo.completed }"
-          >
-            {{ todo.subject }}
-          </span>
+      <template #default="{ item, index }">
+        <div 
+          class="card-body p-2 d-flex align-items-center"
+          style="cursor: pointer"
+          @click="moveToPage(item.id)"
+        >
+          <div class="flex-grow-1">
+            <input
+              class="ml-2 mr-2"
+              type="checkbox"
+              :checked="item.completed"
+              @change="toggleTodo(index, $event)"
+              @click.stop
+            >
+            <span 
+              :class="{ todo: item.completed }"
+            >
+              {{ item.subject }}
+            </span>
+          </div>
+          <div>
+            <button 
+              class="btn btn-danger btn-sm"
+              @click.stop="openModal(item.id)"
+            >
+              Delete
+            </button>
+          </div>
         </div>
-        <div>
-          <button 
-            class="btn btn-danger btn-sm"
-            @click.stop="openModal(todo.id)"
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
-    <teleport to="#modal">
-      <Modal 
-        v-if="showModal"  
-        @close="closeModal"
-        @delete="deleteTodo"
-      />
-    </teleport>
+      </template>
+    </List>
+  <!-- </div> -->
+
+  <teleport to="#modal">
+  <Modal 
+      v-if="showModal"  
+      @close="closeModal"
+      @delete="deleteTodo"
+    />
+  </teleport>
 </template>
 
 <script>
 import { useRouter } from 'vue-router';
 import Modal from '@/components/DeleteModal.vue';
 import { ref } from 'vue';
+import List from '@/components/List.vue';
 
 export default {
   components: {
-    Modal
+    Modal,
+    List
   },
   props: {
       todos: {
